d578947cc86af7dc9e655f9ab7a5038eb0f1fd2a
Sunny Ripert <negatif@gmail.com>
2008-12-14 00:35:50 +0100

    Better error handling, refactorizatin into a method

--- git-ical.rb
+++ git-ical.rb
@@ -1,20 +1,35 @@
+#!/usr/bin/env ruby
+# $ sudo gem install icalendar grit mime-types
+# $ ruby git-ical.rb git-directory
+
 require 'rubygems'
 require 'icalendar'
 require 'grit'
 
-repo = Grit::Repo.new ARGV.first
+def git_to_ical(dir)
+  repo = Grit::Repo.new(dir)
+  cal = Icalendar::Calendar.new
 
-cal = Icalendar::Calendar.new
-
-repo.commits.each do |commit|
-  cal.event do
-    dtstart     commit.comitted_date
-    dtend       commit.comitted_date
-    summary     "Commit by #{commit.author}"
-    description commit.message
-    klass       "PUBLIC"
+  repo.commits.each do |commit|
+    cal.event do
+      dtstart     commit.committed_date
+      dtend       commit.committed_date
+      summary     "Commit by #{commit.author}"
+      description commit.message
+      klass       "PUBLIC"
+    end
   end
+
+  cal.to_ical
 end
 
-puts cal.to_ical
+if __FILE__ == $0
+  dir = ARGV.first || ""
+  begin
+    puts git_to_ical(dir)
+  rescue Grit::NoSuchPathError, Grit::InvalidGitRepositoryError
+    abort "#{$0}: #{File.expand_path(dir)} is an invalid git directory\n" + \
+          "Usage: #{$0} directory"
+  end
+end
 
